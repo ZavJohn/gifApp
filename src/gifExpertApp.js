36934333e6608d75716ff0df54792ac9a3ee61cb
Jonahan Zavala <dabarelyon@yahoo.com.mx>
2022-03-11 14:59:30 -0600

    inicialización del proyecto

---
+++ src/gifExpertApp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GifExpertApp = () =>{
+
+    return (
+        <>
+            <h2> GifExpertApp </h2>
+            <hr />
+        </>
+        
+    );
+}
+
+/*GifExpertApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+};*/
+
+/*GifExpertApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+};*/
+export default GifExpertApp;
